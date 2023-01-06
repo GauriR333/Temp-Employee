ded73d63c11d75ae4ed144ab9ce8332ebe9c485a
Gauri Ragade <gauriragade98@gmail.com>
2023-01-06 22:18:41 +0530

    Empolyee commit

---
+++ Empoyee/src/com/gauri/Employee.java
@@ -0,0 +1,20 @@
+package com.gauri;
+
+public class Employee {
+	public static void main(String args[]) {
+		 int id;
+		 String name;
+		 int salary;
+		 String address;
+	
+//	Employee(){
+//		id=00;
+//		name="not given";
+//		salary=00;
+//		address="not given"
+//	}
+//	void display() {
+		System.out.println("id,name,salary,address");
+	
+	}
+}
---
+++ Empoyee/src/com/gauri/New_employee.java
@@ -0,0 +1,8 @@
+package com.gauri;
+
+public class New_employee {
+	public static void main(String args[]){
+	 System.out.println("new Employee");	
+	}
+
+}
